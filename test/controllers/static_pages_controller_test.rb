1c59e206b6b42933a9bd6c54819bfee68a609b99
Brett Taylor <sweet.brett@gmail.com>
2016-10-11 18:54:06 +0000

    update title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  def setup
-    @base_title = "The DW"
-  end
-  
   test "should get root" do
     get root_url
     assert_response :success
@@ -14,19 +10,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "The DW"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | The DW"
   end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | The DW"
   end
 
 end
